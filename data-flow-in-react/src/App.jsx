6ca8b857de1e3d869bf1ee7d63510a474fd0bdd5
Mahou25 <mahougnon.yanclo@gmail.com>
2025-11-15 09:18:03 +0100

    data flow in react setter

--- data-flow-in-react/src/App.jsx
+++ data-flow-in-react/src/App.jsx
@@ -3,32 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function CGUCheckbox({ checked, onCheck }) {
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => onCheck(e.target.checked)}
+        />
+        Accepter les conditions d'utilisation
+      </label>
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [isTermAccepted, setIsTermAccepted] = useState(false)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form>
+      <CGUCheckbox checked={isTermAccepted} onCheck={setIsTermAccepted} />
+      <button disabled={!isTermAccepted}>Envoyer le formulaire</button>
+    </form>
   )
 }
 
