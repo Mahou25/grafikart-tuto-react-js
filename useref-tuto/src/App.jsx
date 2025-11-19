c53fda4d8df622e55e552a976608443def023fb6
Mahou25 <mahougnon.yanclo@gmail.com>
2025-11-19 17:45:02 +0100

    usage de forwardRef

--- useref-tuto/src/App.jsx
+++ useref-tuto/src/App.jsx
@@ -1,68 +1,65 @@
 import './App.css';
 
-import {
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import { useRef } from 'react';
 
 import { Input } from '../../usememo-tuto/src/components/Input';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const prefixRef = useRef(null)
-  //useRef(null) = useMemo(()=>{(current: null)}, [])
-  const [prefix, setPrefix]=useState('')
-  prefixRef.current = prefix
+// function App() {
+//   const [count, setCount] = useState(0)
+//   const prefixRef = useRef(null)
+//   //useRef(null) = useMemo(()=>{(current: null)}, [])
+//   const [prefix, setPrefix]=useState('')
+//   prefixRef.current = prefix
 
   
-  console.log(ref)
+//   console.log(ref)
   
-  // useEffect(()=>{
-  //     console.log(ref.current.offsetHeight)
-  //   }, []);
+//   // useEffect(()=>{
+//   //     console.log(ref.current.offsetHeight)
+//   //   }, []);
 
 
-    useEffect(()=>{
-      const timer = setInterval(()=>{
-        console.log(prefixRef.current)
-      }, 1000)
-      return()=>{
-        clearInterval(timer)
-      }
-    }, []);
+//     useEffect(()=>{
+//       const timer = setInterval(()=>{
+//         console.log(prefixRef.current)
+//       }, 1000)
+//       return()=>{
+//         clearInterval(timer)
+//       }
+//     }, []);
 
-  return (
+//   return (
 
 
-    // <div onClick={() => ref.current='hello' }>
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-    //   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-    //   nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-    //   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-    //   eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-    //   in culpa qui officia deserunt mollit anim id est laborum.
-    // </div>
+//     // <div onClick={() => ref.current='hello' }>
+//     //   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+//     //   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+//     //   nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+//     //   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+//     //   eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+//     //   in culpa qui officia deserunt mollit anim id est laborum.
+//     // </div>
 
     
 
 
-    <div ref={ref}>
-      <Input label="prefix" value={prefix} onChange={setPrefix} />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-      eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-      in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
+//     <div ref={ref}>
+//       <Input label="prefix" value={prefix} onChange={setPrefix} />
+//       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+//       incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
+//       nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+//       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+//       eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+//       in culpa qui officia deserunt mollit anim id est laborum.
+//     </div>
 
 
-  )
-}
+//   )
+// }
 
-function AppSecond() {
+function App() {
   const ref = useRef()
+  console.log('App',ref)
   // const [prefix, setPrefix]=useState('')
   // prefixRef.current = prefix
   
@@ -76,7 +73,7 @@ function AppSecond() {
   //   }, []);
 
   return <div>
-    <Input label="prefix" value={prefix} onChange={setPrefix} />
+    <Input inputRef={ref} label="prefix"  />
   </div>
 
 }
--- useref-tuto/src/components/Input.jsx
+++ useref-tuto/src/components/Input.jsx
@@ -1,24 +1,39 @@
-import { useId } from 'react';
-
 /**
  * 
  * @param {String} placeholder
  * @param {String} value
  * @param {(s:string) => void} onChange
  */
-export function Input({placeholder, value, onChange, label, ref}) {
-    console.log(ref)
-    const id = useId()
-    return <div>
-        <label className="form-label" htmlFor={id} > {label} </label>
-        <input
-            ref={ref}
-            id={id}
-            type="text"
-            className="form-control"
-            value={value}
-            placeholder={placeholder}
-            onChange={(e) => onChange(e.target.value)}
-        />
-    </div>
-}
+// export function Input({placeholder, value, onChange, label, inputRef, wrapperRef, labelRef}) {
+//     console.log('input',inputRef)
+//     const id = useId()
+//     return <div ref={wrapperRef}>
+//         <label ref={labelRef}  className="form-label" htmlFor={id} > {label} </label>
+//         <input
+//             ref={inputRef}
+//             id={id}
+//             type="text"
+//             className="form-control"
+//             value={value}
+//             placeholder={placeholder}
+//             onChange={(e) => onChange?.(e.target.value)}
+//         />
+//     </div>
+// }
+
+
+export const Input = forwardRef(function Input ({placeholder, value, onChange, label}, ref) {
+  console.log(ref)
+  const id = useId()
+  return <div>
+    <label className="form-label" htmlFor={id}>{label}</label>
+    <input
+      ref={ref}
+      id={id}
+      className="form-control"
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => onChange?.(e.target.value)}
+    />
+  </div>
+})
