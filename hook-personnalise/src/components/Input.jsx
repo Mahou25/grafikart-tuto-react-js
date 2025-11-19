21578b59d4ead3a2c8638e386dac9aee959b0a14
Mahou25 <mahougnon.yanclo@gmail.com>
2025-11-19 19:03:49 +0100

    hook personnalisé compliqué

---
+++ hook-personnalise/src/components/Input.jsx
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @param {String} placeholder
+ * @param {String} value
+ * @param {(s:string) => void} onChange
+ */
+export function Input({placeholder, value, onChange}) {
+    return <div>
+        <input
+            type="text"
+            className="form-control"
+            value={value}
+            placeholder={placeholder}
+            onChange={(e) => onChange(e.target.value)}
+        />
+    </div>
+}
