7e8c1c073f18331fe38bf2eceb672a1c817d5401
Mahou25 <mahougnon.yanclo@gmail.com>
2025-11-19 19:34:37 +0100

    combinaison de use state, useref, usememo, use effect

--- hook-personnalise/src/App.jsx
+++ hook-personnalise/src/App.jsx
@@ -2,16 +2,30 @@ import './App.css';
 
 import { useState } from 'react';
 
-import { useToggle } from './hooks/useToggle'
+import { useToggle } from './hooks/useToggle.js';
 
-import { useIncrement } from './hooks/useIncrement'
+import { useIncrement } from "./hooks/useIncrement.js";
+
+import { useDocumentTitle } from "./hooks/useDocumentTitle.js";
+
+import { Input } from './components/Input';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   // const [checked, toggleCheck]= useToggle()
 
-  const[count, increment, decrement]= useIncrement(0)
+  // const { count, increment, decrement } = useIncrement(0)
+
+  const{count,increment,  decrement} = useIncrement({
+    base: 0,
+    max: 10,
+    min:0
+  })
+
+  const[name, setName] = useState('')
+
+  useDocumentTitle(name ? 'Editer ${name}' : null)
 
 //dès qu'une fonction utilise un hook, elle devient elle même un hook, il faudra penser à les préfixer par un mot clé use pour pouvoir les identifier après
 
@@ -20,6 +34,7 @@ function App() {
         {/* <input type="checkbox" checked={checked} onChange={toggleCheck} />
         {checked && 'Je suis coché'} */}
 
+        <Input value={name} onChange={setName} label="Nom" />
         Compteur {count}
         <button onClick={increment} >Incrémenter</button>
         <button onClick={decrement} >Décrémenter</button>
--- hook-personnalise/src/hooks/useIncrement.js
+++ hook-personnalise/src/hooks/useIncrement.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-export function useIncrement(initialValue = 0) {
-    const [state, setState] = useState(initialValue)
+export function useIncrement({base = 0, max=Infinity, min=-Infinity}) {
+    const [state, setState] = useState(base)
     return {
         count: state,
-        increment: ()=> setState(v => v+1),
-        decrement: ()=>setState(v => v-1)
+        increment: ()=> setState(v => v < max ? v+1 : v),
+        decrement: ()=>setState(v => v > min ? v-1 : v)
     }
 }
