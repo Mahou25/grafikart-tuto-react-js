c6ff64a8d22f8f1b80f68903e8da81e891b3965c
Mahou25 <mahougnon.yanclo@gmail.com>
2025-11-19 18:10:29 +0100

    beginning of hook personnalisé

--- useeffect-tuto/src/components/Input.jsx
+++ useeffect-tuto/src/components/Input.jsx
@@ -4,7 +4,7 @@
  * @param {String} value
  * @param {(s:string) => void} onChange
  */
-export function Input({placeholder, value, onChange}) {
+export function Input({placeholder, value, onChange,}) {
     return <div>
         <input
             type="text"
--- usememo-tuto/src/App.jsx
+++ usememo-tuto/src/App.jsx
@@ -1,24 +1,33 @@
 import './App.css';
 
-import { useState } from 'react';
+import {
+  useMemo,
+  useState,
+} from 'react';
 
-import { Input } from './components/forms/Input.jsx';
+import { Input } from './components/Input';
 
 function App() {
   const [firstname, setFirstname] = useState('John');
   const [password, setPassword] = useState('MotDePasse');
   // const security = passwordSecurity(password);
-  const security = useMemo(()=>{
-    console.log('hello')
-    return passwordSecurity(password)
-  }, [password])
+  // const security = useMemo(()=>{
+  //   console.log('hello')
+  //   return passwordSecurity(password)
+  // }, [password])
+  
+  const security = passwordSecurity(password)
+ 
+
+  const random = useMemo(()=>Math.random(),[])
 
 
-  console.log(useId())
+  // console.log(useId())
 
 
   return (
     <div className="container my-3 vstack gap-2">
+      {random}
       <Input
         label="Nom d'utilisateur"
         value={firstname}
