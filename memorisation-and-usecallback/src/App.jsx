cd515a7239b2ccabeeff086f2d6d467ce55c58f0
Mahou25 <mahougnon.yanclo@gmail.com>
2025-11-20 14:22:19 +0100

    utilisation avancée de callback

--- memorisation-and-usecallback/src/App.jsx
+++ memorisation-and-usecallback/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import {
+  useMemo,
+  useState,
+} from 'react';
+
+import Input from './components/Input.jsx';
+// import Input from './components/Input.jsx';
+import { waitSync } from './utils/waitSync.js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  // const handleClick = () => {
+  //   console.log('hello')
+  // }
+  
+  // const handleClick =useMemo (() => {
+  //   console.log('hello')
+  // }, [])
+  // 
+  
+  const handleClick =useCallback (() => {
+    console.log('name.length')
+  }, [name.length])
+
+  const a= useMemo({a:3}, [])
+
+  // console.log(<div className='container my-2 vstack gap-2'> 
+  //     <div>
+  //       <Input label = "Prénom" onChange={setName} value={name} />
+  //       <div>
+  //         {name.toUpperCase()}
+  //       </div>
+  //     </div>
+  //     <Info/>
+
+  //   </div>)
+
+  console.log('App', 'render')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container my-2 vstack gap-2'> 
+      <Demo/>
+      <InfoMemo onClick = {handleClick} a={a} />
+    </div>
   )
 }
+
+function Demo() {
+  const [name, setName] = useState('')
+    return (
+      <div>
+        <Input label = "Prénom" onChange={setName} value={name} />
+        <div>
+          {name.toUpperCase()}
+        </div>
+      </div>
+  )
+}
+
+const InfoMemo = memo(function Info({onClick}) {
+  console.log('Info', 'render')
+
+waitSync(500)
+return <div className="alert alert-info" onClick = {onClick} >
+    {name}
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+    exercitation ullamco laboris n
+  </div>
+}
+)
+
 
 export default App
